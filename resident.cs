97488c18e1df4b958a53ea11ec551df913a3a424
Joshua Aquino <email@joshuaaquino.xyz>
2022-03-18 16:08:56 -0400

    add sign to incRelation and small insult function

--- resident.cs
+++ resident.cs
@@ -6,7 +6,8 @@ public class resident : Node
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-    private const short MAX_RELATION = 100;
+    private const byte MAX_RELATION = 100;
+    private const byte MIN_RELATION = 0;
     public short number
     {get; set;}
     public string name
@@ -15,18 +16,31 @@ public class resident : Node
     // index is resNumber; relation is 0-100
     // figure out get/set?
     // get and set logic for this should be placed in the method that alters this
-    public void incRelationsWith(short number, byte increment) {
-        byte sum = this.relationsWith[number];
-        sum += increment;
-        if (sum <= MAX_RELATION)
-        this.relationsWith[number] = sum;
-        else
-        this.relationsWith[number] = 100;
+    public void incRelationsWith(short number, sbyte increment) {
+        sbyte sum = (sbyte)this.relationsWith[number];
+        if (increment > 0) {
+            sum += increment;
+            if (sum <= MAX_RELATION)
+            this.relationsWith[number] = (byte)sum;
+            else
+            this.relationsWith[number] = MAX_RELATION;
+        } else {
+            sum += increment;
+            if (sum >= MIN_RELATION)
+            this.relationsWith[number] = (byte)sum;
+            else
+            this.relationsWith[number] = MIN_RELATION;
+        }
     }
     public resident(short number, string name)
     {
         this.number = number;
         this.name = name;
+    }
+    // when you insult someone, THEY CHANGE THEIR OPINION ON YOU,
+    // each character can only change their own opinions
+    public void insultedBy(short offender) {
+        incRelationsWith(offender, -6);
     }
 
     // Called when the node enters the scene tree for the first time.
--- test.cs
+++ test.cs
@@ -10,9 +10,25 @@ public class test : Node2D
     List<resident> residentList = new List<resident>();
     public void onButtinPresst() {
         residentList[0].incRelationsWith(1, 10);
+        residentList[1].incRelationsWith(0, 10);
+    }
+
+    public void _on_Button2_pressed() {
+        residentList[0].incRelationsWith(1, -10);
+        residentList[1].incRelationsWith(0, 10);
+    }
+
+    public void _on_Button3_pressed() {
         GD.Print(residentList[0].name + " likes " +
         residentList[1].name + " " + residentList[0].relationsWith[1] +
         "%.");
+        GD.Print(residentList[1].name + " likes " +
+        residentList[0].name + " " + residentList[1].relationsWith[0] +
+        "%.");
+    }
+    
+    public void _on_insult_pressed() {
+        residentList[0].insultedBy(1);
     }
 
     // Called when the node enters the scene tree for the first time.
